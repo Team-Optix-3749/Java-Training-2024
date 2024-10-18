f75279ce53a5f6809b1e5404fed07fb1619e1fb5
JustinQ-DNHS <justin.q224@icloud.com>
2024-10-17 17:35:16 -0700

    finished class homework

--- Week3/Classes&Objects/Pokemon.java
+++ Week3/Classes&Objects/Pokemon.java
@@ -2,17 +2,29 @@ public class Pokemon {
     String name;
     int level;
     
+
+    Pokemon(){
+        level = 1;
+    }
+
+    Pokemon(String pName, int pLevel){
+        // Use variables defined in class (at top of page) rather than local variables (this statements)
+        this.name = pName;
+        this.level = pLevel;
+    }
+    
     void attack(){
-        System.out.print(name + "attack!");
+        System.out.println(name + "attack!");
     }
 
     public static void main(String[] args) {
     
-        Pokemon p1 = new Pokemon();
-        p1.name = "Pikachu";
-        p1.level = 100;
+        Pokemon p1 = new Pokemon("Pikachu", 15);
         
-        System.out.println(p1.name + " " + p1.level);
-        p1.attack();
+        System.out.print(p1.name + "\nLevel:" + p1.level);
+
+        Pokemon p2 = new Pokemon("Vaporeon", 69);
+
+        p2.attack();
     }
 }
