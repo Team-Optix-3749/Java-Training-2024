da94ca31f8fee755dec2fae2ad45788d5c4e71e6
JustinQ-DNHS <justin.q224@icloud.com>
2024-10-22 17:13:24 -0700

    added public and private to promote better readability

--- Week3/Abstraction/Shirt.java
+++ Week3/Abstraction/Shirt.java
@@ -1,7 +1,8 @@
 // Abstract means that you can't create a object of this class, only it's children
 public abstract class Shirt {
-    String color;
-    Shirt(String color) {
+    public String color;
+    
+    public Shirt(String color) {
         this.color = color;
     }
 
@@ -20,33 +21,33 @@ public abstract class Shirt {
 }
 
 class T_Shirt extends Shirt {
-    String size;
-    double price;
+    private String size;
+    private double price;
 
-    T_Shirt(String color, String size, double price) {
+    public T_Shirt(String color, String size, double price) {
         super(color);
         this.size = size;
         this.price = price;
     }
     
     @Override
-    void getDescription() {
+    public void getDescription() {
         System.out.println("\n\nT-Shirt:\nColor: " + color + "\nSize: " + size + "\nPrice: $" + price);
     }
 }
 
 class Jacket extends Shirt {
-    double price;
-    String brand;
+    private double price;
+    private String brand;
 
-    Jacket(String color, String brand, double price) {
+    public Jacket(String color, String brand, double price) {
         super(color);
         this.brand = brand;
         this.price = price;
     }
 
     @Override
-    void getDescription() {
+    public void getDescription() {
         System.out.println("\n\nJacket:\nColor: " + color + "\nBraand: " + brand + "\nPrice: $" + price);
     }
 }
